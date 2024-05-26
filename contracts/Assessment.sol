5915642e85736d80df45ef04cb7cc58aea1195fc
NTC_CarmelaLusaya <138741841+mclusaya21@users.noreply.github.com>
2024-05-26 23:59:13 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -23,23 +23,23 @@ contract Lusaya {
         uint _previousBalance = balance;
 
         // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
+        require(msg.sender == owner, "Only owner can perform this transaction ");
 
-        // perform transaction
+        
         balance += _amount;
 
-        // assert transaction completed successfully
+        
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
+        
         emit Deposit(_amount);
     }
 
-    // custom error
+    
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+        require(msg.sender == owner, "Only owner can perform this transaction ");
         uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
@@ -48,13 +48,13 @@ contract Lusaya {
             });
         }
 
-        // withdraw the given amount
+        
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
+        
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
+        
         emit Withdraw(_withdrawAmount);
     }
 }
