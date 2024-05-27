7c7d11ca18f1955aee20eed908ed1598b7404382
NTC_CarmelaLusaya <138741841+mclusaya21@users.noreply.github.com>
2024-05-27 09:00:16 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Lusaya {
     address payable public owner;
     uint256 public balance;
@@ -10,51 +8,43 @@ contract Lusaya {
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
 
-    constructor(uint initBalance) payable {
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() external view returns (uint256) {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+    function deposit(uint256 amount) external payable {
+        require(msg.sender == owner, "Only the owner can perform this transaction");
+        uint256 previousBalance = balance;
 
-        
-        require(msg.sender == owner, "Only owner can perform this transaction ");
+        balance += amount;
 
-        
-        balance += _amount;
+        assert(balance == previousBalance + amount);
 
-        
-        assert(balance == _previousBalance + _amount);
-
-        
-        emit Deposit(_amount);
+        emit Deposit(amount);
     }
 
-    
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "Only owner can perform this transaction ");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
+    function withdraw(uint256 withdrawAmount) external {
+        require(msg.sender == owner, "Only the owner can perform this transaction");
+        uint256 previousBalance = balance;
+
+        if (balance < withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
-                withdrawAmount: _withdrawAmount
+                withdrawAmount: withdrawAmount
             });
         }
 
-        
-        balance -= _withdrawAmount;
+        balance -= withdrawAmount;
 
-        
-        assert(balance == (_previousBalance - _withdrawAmount));
+        assert(balance == previousBalance - withdrawAmount);
 
-        
-        emit Withdraw(_withdrawAmount);
+        emit Withdraw(withdrawAmount);
     }
 }
